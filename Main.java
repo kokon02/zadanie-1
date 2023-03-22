b5acc703b700fafdbd9a8477514fc2dd5752e04f
kokon02 <127319252+kokon02@users.noreply.github.com>
2023-03-22 21:32:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    String string = "Hello world!";
+    for(int i=0; i<string.length(); i ++){
+      System.out.println(string.charAt(i)); 
+    }
+      
+    
+    
+    
+  }
+}
